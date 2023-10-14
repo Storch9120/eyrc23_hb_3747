34259940833775974b686157f36297accd387c08
Storch9120 <52457425+Storch9120@users.noreply.github.com>
2023-10-14 13:04:12 +0530

    Add files via upload

--- controller.py
+++ controller.py
@@ -9,6 +9,13 @@ from my_robot_interfaces.srv import NextGoal
 import time
 import math
 
+##ssharma 10-10-23
+## Obj: To correct the service response parsing. 
+# Program Desc: The controller pushes twist to /cmd_vel  and P controller uses feedback 
+# from /odom. The service NextGoal is requested to set the goal which the bot 
+# is to achieve. 
+
+
 class HBTask1BController(Node):
     def __init__(self):
         super().__init__('hb_task1b_controller')
@@ -31,11 +38,11 @@ class HBTask1BController(Node):
         self.hb_y = 0
         self.hb_theta = 0
 
-        self.x_goals = [4, -4, -4, 4, 0]
-        self.y_goals = [4, 4, -4, -4, 0]
-        self.theta_goals = [0, math.pi/2, math.pi, -math.pi/2, 0]
+        # self.x_goals = [4, -4, -4, 4, 0]
+        # self.y_goals = [4, 4, -4, -4, 0]
+        # self.theta_goals = [0, math.pi/2, math.pi, -math.pi/2, 0]
 
-        self.index = 0
+        
         self.rate = self.create_rate(100)
 
         # Create a client for the "next_goal" service
@@ -45,12 +52,14 @@ class HBTask1BController(Node):
             self.get_logger().info('Service "next_goal" not available, waiting...')
 
         self.req = NextGoal.Request()
+        self.index = 0
 
     def odometry_callback(self, msg):
+        print("odom was called")
         orientation = msg.pose.pose.orientation
         (roll, pitch, yaw) = euler_from_quaternion(
             [orientation.x, orientation.y, orientation.z, orientation.w])
-
+        
         self.hb_x = msg.pose.pose.position.x
         self.hb_y = msg.pose.pose.position.y
         self.hb_theta = yaw
@@ -58,22 +67,19 @@ class HBTask1BController(Node):
     def send_request(self, index):
         self.req.request_goal = index
         future = self.next_goal_client.call_async(self.req)
+        rclpy.spin_once(self)
+        if future.done():
+            try:
+                response = future.result()
+                return response
+            except Exception as e:
+                self.get_logger().info(f'Service call failed: {e}')
+                return None
 
-        while rclpy.ok():
-            rclpy.spin_once(self)
-            if future.done():
-                try:
-                    response = future.result()
-                    return response
-                except Exception as e:
-                    self.get_logger().info(f'Service call failed: {e}')
-                    return None
-
-    def control_loop(self):
-        while rclpy.ok():
-            x_goal = self.x_goals[self.index]
-            y_goal = self.y_goals[self.index]
-            theta_goal = self.theta_goals[self.index]
+    def control_loop(self, x, y, theta):
+            x_goal = x
+            y_goal = y
+            theta_goal = theta
 
             error_x = x_goal - self.hb_x
             error_y = y_goal - self.hb_y
@@ -91,21 +97,45 @@ class HBTask1BController(Node):
             self.cmd_vel_publisher.publish(self.vel)
 
             # Check if the goal has been reached
-            if abs(error_x) < 0.1 and abs(error_y) < 0.1 and abs(error_theta) < 0.1:
+            if (abs(error_x) < 0.1 and abs(error_y) < 0.1 and abs(error_theta) < 0.1):
                 # Stay at the goal pose for at least 1 second
                 time.sleep(1)
-                self.index += 1
-                if self.index >= len(self.x_goals):
-                    self.index = 0
-
+                # self.index += 1
+                # if self.index >= len(self.x_goals):
+                #     self.index = 0
             self.rate.sleep()
 
 def main(args=None):
     rclpy.init(args=args)
-    controller = HBTask1BController()
-    controller.control_loop()
-    rclpy.spin(controller)
-    controller.destroy_node()
+    ebot_controller = HBTask1BController()
+# Send an initial request with the index from ebot_controller.index
+    ebot_controller.send_request(ebot_controller.index)
+
+    while rclpy.ok():
+        #check if service call is done
+        if ebot_controller.future.done():
+            try:
+                response = ebot_controller.future.result()
+            except Exception as e:
+                ebot_controller.get_logger().infselfo('Service call failed %r' % (e,))
+            else:
+            #########           GOAL POSE             #########
+                x_goal      = response.x_goal
+                y_goal      = response.y_goal
+                theta_goal  = response.theta_goal
+                ebot_controller.flag = response.end_of_list
+            ####################################################
+
+            ############     DO NOT MODIFY THIS       #########
+                ebot_controller.index += 1
+                if ebot_controller.flag == 1 :
+                    ebot_controller.index = 0
+                ebot_controller.send_request(ebot_controller.index)
+            ####################################################
+    # controller.control_loop()
+    # rclpy.spin(controller)
+    rclpy.spin_once(ebot_controller)
+    ebot_controller.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
